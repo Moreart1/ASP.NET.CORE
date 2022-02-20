2093bdd7a52fc2708e2193096862101ffd60d265
Moreart1 <morearti1726263@yandex.ru>
2022-02-20 13:02:42 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Lesson1/Controllers/CrudController.cs
@@ -0,0 +1,97 @@
+﻿using Lesson1.Entity;
+using Lesson1.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1.Controllers
+{
+    [ApiController]
+    [Route("api/weather")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly ILogger<WeatherController> _logger;
+        private readonly WeatherRepository _repository;
+
+        public WeatherController(WeatherRepository repository, ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// Возможность сохранить температуру в указанное время
+        /// </summary>
+        /// <param name="date">Дата и время наблюдения</param>
+        /// <param name="temperature">Температура</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            if (_repository.TryAdd(new WeatherEntity { Temperature = temperature, Date = date }))
+            {
+                return Ok("Данные успешно сохранены");
+            }
+            return BadRequest("Не удалось сохранить");
+        }
+
+        /// <summary>
+        /// Возможность отредактировать показатель температуры в указанное время
+        /// </summary>
+        /// <param name="date">Дата и время наблюдения</param>
+        /// <param name="temperature">Температура</param>
+        /// <returns></return
+        [HttpPut]
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int temperature)
+        {
+            if (_repository.TryUpdate(new WeatherEntity { Temperature = temperature, Date = date }))
+            {
+                return Ok("Данные успешно обновлены");
+            }
+            return BadRequest("Не удалось обновить данные");
+        }
+
+        /// <summary>
+        /// Возможность удалить показатель температуры в указанный промежуток времени
+        /// </summary>
+        /// <param name="dateFrom">Начало периода</param>
+        /// <param name="dateTo">Окончание периода</param>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] DateTime? dateFrom, [FromQuery] DateTime? dateTo)
+        {
+
+            if (dateFrom == null || dateTo == null)
+            {
+                return BadRequest("Некорректный диапазон");
+            }
+
+            if (dateFrom > dateTo)
+            {
+                return BadRequest("Некорректный диапазон");
+            }
+
+            if (_repository.Delete((DateTime)dateFrom, (DateTime)dateTo))
+            {
+                return Ok("Данные успешно удалены");
+            }
+            return BadRequest("Не удалось удалить данные");
+        }
+
+        /// <summary>
+        /// Возможность прочитать список показателей температуры за указанный промежуток времени
+        /// </summary>
+        /// <param name="dateFrom">Начало периода</param>
+        /// <param name="dateTo">Окончание периода</param>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<WeatherEntity> Get([FromQuery] DateTime? dateFrom, [FromQuery] DateTime? dateTo)
+        {
+            return _repository.GetWeatherByPeriod(dateFrom, dateTo);
+        }
+    }
+}
