c25dfb824ae84f1e93887172f08eb57ab33294b0
Moreart1 <morearti1726263@yandex.ru>
2022-02-24 15:25:13 +0300

    Доработка проекта

---
+++ MetricsManager/MetricsManager/Model/AgentInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MetricsManager.Model
+{
+    public class AgentInfo
+    {
+        public int AgentId { get; }
+
+        public Uri AgentAddress { get; }
+    }
+}
---
+++ MetricsManager/MetricsManager/Model/AgentsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MetricsManager.Model
+{
+    public class AgentsModel
+    {
+        private readonly List<AgentInfo> _data;
+
+        public AgentsModel()
+        {
+            _data = new List<AgentInfo>();
+        }
+
+        public IReadOnlyList<AgentInfo> GetAgentsInfo()
+        {
+            return _data;
+        }
+    }
+}
