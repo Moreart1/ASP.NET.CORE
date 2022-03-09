aeccce0dc4b59d82cf77ede1a884891281dc1252
Moreart1 <morearti1726263@yandex.ru>
2022-03-09 14:55:52 +0300

    Lesson 5

---
+++ MetricsManager/MetricsAgent/Jobs/Job/RamMetricJob.cs
@@ -0,0 +1,29 @@
+﻿using MetricsAgent.Interface;
+using MetricsAgent.Models;
+using Quartz;
+using System.Diagnostics;
+
+namespace MetricsAgent.Jobs.Job
+{
+    public class RamMetricJob: IJob
+    {
+        private IRamMetricsRepository _repository;
+       private PerformanceCounter _ramCounter;
+
+        public RamMetricJob(IRamMetricsRepository repository)
+        {
+            _repository = repository;
+            _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _repository.Create(new RamMetric
+            {
+                Value = Convert.ToInt32(_ramCounter.NextValue()),
+                Time = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
