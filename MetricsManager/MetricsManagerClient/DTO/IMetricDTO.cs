d46ef1892ba1b9fdb994684036244fd1e40724fe
Moreart1 <morearti1726263@yandex.ru>
2022-04-01 13:07:10 +0300

    Lesson 8

---
+++ MetricsManager/MetricsManagerClient/DTO/IMetricDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsManagerClient.DTO
+{
+    public interface IMetricDTO
+    {
+        int Id { get; set; }
+
+        int Value { get; set; }
+
+        DateTime Date { get; set; }
+    }
+}
